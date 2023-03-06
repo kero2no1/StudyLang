0753fd1ede30040f3affdd508c87a93d456b3141
Kero.T <kerokero.2no1@gmail.com>
2023-03-06 21:49:31 +0900

    Java用ブランチを切って演習実施

---
+++ project/1.10_文字列とキャラクタ/java/java_ex_1_10_2/src/java_ex_1_10_2/Ex_1_10_2.java
@@ -0,0 +1,16 @@
+package java_ex_1_10_2;
+
+public class Ex_1_10_2 {
+
+	public static void main(String[] args) {
+		String myName, s1, s2;
+		
+		s1 = "Keroichi";
+		s2 = "T";
+		myName = s1 + " " + s2;
+		
+		System.out.println( myName );
+
+	}
+
+}
---
+++ project/1.11_算術演算子と式/java/java_ex_1_11_2/src/java_ex_1_11_2/Ex_1_11_2.java
@@ -0,0 +1,15 @@
+package java_ex_1_11_2;
+
+public class Ex_1_11_2 {
+
+	public static void main(String[] args) {
+		double distance = 3000;
+		double speed = 75;
+		
+		System.out.println( "NewYorkからLosAngelesまで " + 
+							distance/speed + " 時間です。" );
+		
+		
+	}
+
+}
