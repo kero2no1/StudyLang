86b790537e7da035bdd9b0137d1a60d78a5da1b8
kero2no1 <kerokero.2no1@gmail.com>
2023-03-07 13:53:27 +0900

    1.12 整数拡張に関するあれこれを検証

--- project/1.11_算術演算子と式/java/java_ex_1_11_2/src/java_ex_1_11_2/Ex_1_11_2.java
+++ project/1.11_算術演算子と式/java/java_ex_1_11_2/src/java_ex_1_11_2/Ex_1_11_2.java
@@ -5,7 +5,7 @@ public class Ex_1_11_2 {
 	public static void main(String[] args) {
 		double distance = 3000;
 		double speed = 75;
-		
+
 		System.out.println( "NewYorkからLosAngelesまで " + 
 							distance/speed + " 時間です。" );
 		
