321b1cb10f04d56fb2e64a29193fcb0e8a32da77
kero2no1 <kerokero.2no1@gmail.com>
2023-03-08 15:34:11 +0900

    Python演習用ブランチ作成 1.10演習

---
+++ project/1.10_文字列とキャラクタ/py/py_ex_1_10_2/src/ex_1_10_2.py
@@ -0,0 +1,15 @@
+'''
+Created on 2023/03/08
+
+@author: Keroichi.T
+'''
+
+def main():
+    s1:str = "Keroichi" 
+    s2:str = "T"
+    myName:str = s1 + " " + s2
+    
+    print( myName )
+
+if __name__ == '__main__':
+    main()
