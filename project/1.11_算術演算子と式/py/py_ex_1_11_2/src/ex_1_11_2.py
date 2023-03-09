2fa387b8f1773f0c2c4ec44cfc16053c4b24a081
kero2no1 <kerokero.2no1@gmail.com>
2023-03-09 14:29:37 +0900

    py演習追加(1.14まで)

---
+++ project/1.11_算術演算子と式/py/py_ex_1_11_2/src/ex_1_11_2.py
@@ -0,0 +1,14 @@
+'''
+Created on 2023/03/09
+
+@author: Keroichi.T
+'''
+
+def main():
+    distance = 3000
+    speed = 75    
+    
+    print("NewYorkからLosAngelesまで {} 時間です。".format(distance/speed))
+
+if __name__ == '__main__':
+    main()
