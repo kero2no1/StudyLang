29eff6964a93a762df9df92ecf55c8f99b0eb5e7
kero2no1 <kerokero.2no1@gmail.com>
2023-03-10 22:17:55 +0900

    リスト(配列)の演習

---
+++ project/1.15_1次元配列/py/py_ex_1_15_2/src/ex_1_15_2.py
@@ -0,0 +1,13 @@
+'''
+Created on 2023/03/10
+
+@author: Keroichi.T
+'''
+
+def main():
+    # 各要素に型の一貫性は必要ない
+    arrData = [ 1, "2", 3.0 ]
+
+    print( arrData )
+if __name__ == '__main__':
+    main()
