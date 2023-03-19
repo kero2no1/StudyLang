1aa7e100c47d01d25fc62025fe98b8365149974f
kero2no1 <kerokero.2no1@gmail.com>
2023-03-19 15:54:57 +0900

    演習2.2を実施

---
+++ project/2.02_静的メソッドと静的変数の概要/py/py_ex_2_02_1/src/ex_2_02_1.py
@@ -0,0 +1,24 @@
+'''
+Created on 2023/03/18
+
+@author: Keroichi.T
+'''
+
+# 標準モジュール、importして使う
+
+# mathモジュール全体を取り込む場合
+import math
+
+# 特定の関数やクラスを取り込む場合
+from math import sqrt
+
+
+def main():
+    print(sqrt(23.45))
+    
+    print(math.pi)
+    
+    
+
+if __name__ == '__main__':
+    main()
---
+++ project/2.02_静的メソッドと静的変数の概要/py/py_ex_2_02_3/src/ex_2_02_3.py
@@ -0,0 +1,14 @@
+'''
+Created on 2023/03/19
+
+@author: Keroichi.T
+'''
+
+import math
+
+def main():
+    print(math.sqrt(math.pow(4.5, 2) + math.pow(8.9, 2)))
+    
+
+if __name__ == '__main__':
+    main()
