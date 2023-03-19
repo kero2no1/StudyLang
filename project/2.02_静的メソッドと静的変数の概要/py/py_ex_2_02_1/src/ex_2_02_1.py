9c68e06b079d370890a183bdce109921606fef5b
kero2no1 <kerokero.2no1@gmail.com>
2023-03-19 21:34:59 +0900

    コメント修正

--- project/2.02_静的メソッドと静的変数の概要/py/py_ex_2_02_1/src/ex_2_02_1.py
+++ project/2.02_静的メソッドと静的変数の概要/py/py_ex_2_02_1/src/ex_2_02_1.py
@@ -4,12 +4,16 @@ Created on 2023/03/18
 @author: Keroichi.T
 '''
 
-# 標準モジュール、importして使う
+# mathモジュールは、静的メソッド(組込み関数)を持ってないみたい
+# ちなみに、printは組込み関数なのでなにもせずに使えている。
+
+# 標準モジュールをimportして使う
 
 # mathモジュール全体を取り込む場合
 import math
 
 # 特定の関数やクラスを取り込む場合
+# この場合、モジュール名を省略することができる
 from math import sqrt
 
 
--- project/2.02_静的メソッドと静的変数の概要/py/py_ex_2_02_2/src/ex_2_02_2.py
+++ project/2.02_静的メソッドと静的変数の概要/py/py_ex_2_02_2/src/ex_2_02_2.py
@@ -4,10 +4,10 @@ Created on 2023/03/19
 @author: Keroichi.T
 '''
 
-import math
+from math import pow
 
 def main():
-    print("2の12乗は" + str(math.pow(2,12)))    
+    print("2の12乗は" + str(pow(2,12)))    
 
 if __name__ == '__main__':
     main()
