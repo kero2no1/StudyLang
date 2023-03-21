806a3a90d73086b8de64cc1c29e56e62f4bfaaf2
kero2no1 <kerokero.2no1@gmail.com>
2023-03-21 18:19:34 +0900

    演習2.5実施、Eclipse環境更新対応

---
+++ project/2.07_その他のラップクラス/java/java_ex_2_07_1/src/java_ex_2_07_1/Ex_2_07_1.java
@@ -0,0 +1,9 @@
+package java_ex_2_07_1;
+
+public class Ex_2_07_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	}
+
+}
---
+++ project/2.07_その他のラップクラス/java/java_ex_2_07_2/src/java_ex_2_07_2/Ex_2_07_2.java
@@ -0,0 +1,10 @@
+package java_ex_2_07_2;
+
+public class Ex_2_07_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
