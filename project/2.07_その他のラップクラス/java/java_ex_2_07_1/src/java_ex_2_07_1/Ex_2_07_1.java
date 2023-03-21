221a633b91b5ce45f05a882ebc6ebe2ed4d767d9
kero2no1 <kerokero.2no1@gmail.com>
2023-03-21 23:09:41 +0900

    演習2.9まで実施

--- project/2.07_その他のラップクラス/java/java_ex_2_07_1/src/java_ex_2_07_1/Ex_2_07_1.java
+++ project/2.07_その他のラップクラス/java/java_ex_2_07_1/src/java_ex_2_07_1/Ex_2_07_1.java
@@ -3,7 +3,15 @@ package java_ex_2_07_1;
 public class Ex_2_07_1 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
+		char ch1 = 'A';
+		char ch2 = '1';
+		
+		System.out.println(Character.isDigit(ch1));
+		System.out.println(Character.isDigit(ch2));
+		
+		System.out.println(Character.toLowerCase(ch1));
+		
 	}
 
 }
--- project/2.07_その他のラップクラス/java/java_ex_2_07_2/src/java_ex_2_07_2/Ex_2_07_2.java
+++ project/2.07_その他のラップクラス/java/java_ex_2_07_2/src/java_ex_2_07_2/Ex_2_07_2.java
@@ -3,8 +3,21 @@ package java_ex_2_07_2;
 public class Ex_2_07_2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
+		// 16進数
+		short va_16 = Short.parseShort("22",16);
+		System.out.println( va_16 );
+		
+		// 8進数
+		short va_8 = Short.parseShort("33",8);
+		System.out.println( va_8 );
+		
+		// 10進数
+		short va_10 = Short.parseShort("44",10);
+		System.out.println( va_10 );
+		
+		// 合計値
+		System.out.println( va_16 + va_8 + va_10 );
 	}
 
 }
