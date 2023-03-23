d6e13f910a63b432cecc86efc26821c1ef0d0137
kero2no1 <kerokero.2no1@gmail.com>
2023-03-23 22:53:14 +0900

    cmathの確認(sqrtとM_PI)

---
+++ project/2.02_静的メソッドと静的変数の概要/cpp/cpp_ex_2_02_1/src/cpp_ex_2_02_1.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : cpp_ex_2_02_1.cpp
+// Author      : keroichi.T
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cmath>		// math.hをラップしたもの
+						// std名前空間内に所属するようになる
+using namespace std;
+
+int main() {
+
+	//
+	cout << "23.45の平方根は：" << sqrt(23.45) << endl;
+	cout << "円周率：" << M_PI << endl;
+
+	return 0;
+}
