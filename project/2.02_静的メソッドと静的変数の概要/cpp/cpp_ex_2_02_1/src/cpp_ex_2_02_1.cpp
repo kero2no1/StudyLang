5f0c437f20a51e3b92f4bf1e291f49727786c253
kero2no1 <kerokero.2no1@gmail.com>
2023-03-24 09:41:17 +0900

    演習2.2まで実施、2.3のプロジェクト作成

--- project/2.02_静的メソッドと静的変数の概要/cpp/cpp_ex_2_02_1/src/cpp_ex_2_02_1.cpp
+++ project/2.02_静的メソッドと静的変数の概要/cpp/cpp_ex_2_02_1/src/cpp_ex_2_02_1.cpp
@@ -9,12 +9,15 @@
 #include <iostream>
 #include <cmath>		// math.hをラップしたもの
 						// std名前空間内に所属するようになる
+#include <iomanip>		// 表示桁設定用(setprecision())
 using namespace std;
 
 int main() {
 
-	//
-	cout << "23.45の平方根は：" << sqrt(23.45) << endl;
+	// 静的メソッドでも静的変数でもない。
+	// グローバルスコープで使えることを考えると同じか？
+	cout << "23.45の平方根は：";
+	cout << std::setprecision(16) << sqrt(23.45) << endl;
 	cout << "円周率：" << M_PI << endl;
 
 	return 0;
