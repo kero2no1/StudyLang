fb9489ff8a2f96ca2b5d32b8a82c86201ff1399c
kero2no1 <kerokero.2no1@gmail.com>
2023-03-25 21:41:09 +0900

    内容補足

--- project/2.03_インスタンスメソッドとインスタンス変数の概要/cpp/cpp_ex_2_03_1/src/cpp_ex_2_03_1.cpp
+++ project/2.03_インスタンスメソッドとインスタンス変数の概要/cpp/cpp_ex_2_03_1/src/cpp_ex_2_03_1.cpp
@@ -7,9 +7,33 @@
 //============================================================================
 
 #include <iostream>
+//#include <string>
+//#include <typeinfo>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	string s = "\"Romeo, where art thou?\"";
+
+	// sizeはlengthと同じ。他のクラスとの互換性のためにsizeが用意されているらしい。
+	// 文字列操作は一般的にはlength使うかな。
+	cout << s.substr(s.size()-10) << endl;
+
+	std::
+
+	// キャストできる→これは暗黙的にコンストラクタが呼ばれてるのかな？
+	cout << ((string)"\"Romeo, where art thou?\"").substr(0,10) << endl;
+
+	// 文字列リテラルはchar型になっているようだ
+	char cp[] = "text";
+	cout << typeid("text").name() << ","	// A:文字列 5文字 char→A5_c
+		 << typeid("test").name() << ","
+		 << typeid(cp).name() << endl;
+
+	cout << typeid(s).name() << ","
+		 << typeid((string)"string").name();
+
+
+
 	return 0;
 }
