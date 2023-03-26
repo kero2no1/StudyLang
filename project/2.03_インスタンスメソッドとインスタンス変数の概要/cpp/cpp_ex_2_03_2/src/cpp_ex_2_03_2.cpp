6cd4ca9bd21f8930d8095eda50376b693aeb2a38
kero2no1 <kerokero.2no1@gmail.com>
2023-03-26 23:12:50 +0900

    演習2.3まで実施

--- project/2.03_インスタンスメソッドとインスタンス変数の概要/cpp/cpp_ex_2_03_2/src/cpp_ex_2_03_2.cpp
+++ project/2.03_インスタンスメソッドとインスタンス変数の概要/cpp/cpp_ex_2_03_2/src/cpp_ex_2_03_2.cpp
@@ -10,6 +10,22 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	string s = "11,22,33,44,55";
+
+	// 後ろから検索
+	int ep = s.rfind(",");
+	cout << s.substr(ep+1) << "\n";
+
+	// 前から検索して、2文字分抽出
+	int fp = s.find(",");
+	cout << s.substr(fp+1,2) << endl;
+
+	// 同じ動作をする、find_first_of関数もある
+	// こちらは文字集合による検索が可能で、
+	// 指定したいずれかの文字が最初に出現する位置を返す
+	fp = s.find_first_of(",4");		// カンマか4を探す
+	cout << fp << endl;
+
+
 	return 0;
 }
