e526f523c7d30bb21b8ab4e902bdb89d969c37c8
kero2no1 <kerokero.2no1@gmail.com>
2023-04-04 22:31:04 +0900

    演習5.2まで実施(java)

---
+++ project/5.02_簡単なクラスの作成/java/java_ex5_02_2/src/java_ex5_02_2/java_ex5_02_2.java
@@ -0,0 +1,22 @@
+package java_ex5_02_2;
+
+class Sphere {
+	double radius;					// 半径
+//	double[] cp = new double[2];	// 中心座標
+	double[] cp;					// 中心座標
+}
+
+public class java_ex5_02_2 {
+
+	public static void main(String[] args) {
+		
+		Sphere cube = new Sphere();
+		cube.radius = 5.5;
+		cube.cp = new double[] {1.2,3.4};
+
+		System.out.println( "半径：" + cube.radius );
+		System.out.println( "中心座標：" + cube.cp[0] + "," +  cube.cp[1] );
+		
+	}
+
+}
