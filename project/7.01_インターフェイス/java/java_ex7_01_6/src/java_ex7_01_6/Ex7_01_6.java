cdfd84a3e82bcb864686e9bfde7d06de9d257937
kero2no1 <kerokero.2no1@gmail.com>
2023-04-15 17:32:50 +0900

    7章演習準備

---
+++ project/7.01_インターフェイス/java/java_ex7_01_6/src/java_ex7_01_6/Ex7_01_6.java
@@ -0,0 +1,10 @@
+package java_ex7_01_6;
+
+public class Ex7_01_6 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ project/7.02_インターフェイスの参照/java/java_ex7_02_2/src/java_ex7_02_2/Ex7_02_2.java
@@ -0,0 +1,10 @@
+package java_ex7_02_2;
+
+public class Ex7_02_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
