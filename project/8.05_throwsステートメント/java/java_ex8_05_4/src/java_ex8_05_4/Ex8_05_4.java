340312307b01a675a31eff8c94276d769b93e727
prof-ker0ke1 <prof.ker0ke1@gmail.com>
2023-04-25 23:12:46 +0900

    演習8.05まで実施(java)

--- project/8.05_throwsステートメント/java/java_ex8_05_4/src/java_ex8_05_4/Ex8_05_4.java
+++ project/8.05_throwsステートメント/java/java_ex8_05_4/src/java_ex8_05_4/Ex8_05_4.java
@@ -1,21 +1,33 @@
 package java_ex8_05_4;
 
+import java.io.IOException;
+
 public class Ex8_05_4 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-//		String[] ss = {"12.3","2.34","345.6","56.7","6.7"};
-		String[] ss = {"abcd","2.34","345.6","56.7","6.7"};
+		String[] ss = {"12.3","2.34","345.6","56.7","6.7"};
+//		String[] ss = {"abcd","2.34","345.6","56.7","6.7"};
 
 		double avg = average( ss );
 //		double avg = average( null );
 
 		System.out.println( avg );
 
+		try {
+			sample();
+		}
+		// sampleメソッドは検査例外を投げるので、捕獲処理が必要になる
+		// 処理がないとコンパイルエラー
+		catch ( IOException e ) {
+			
+		}
+		
 	}
 	
 	public static double average( String[] aryStr ) 
+			// いずれも非検査例外なので、捕獲を強制されない
 			throws NullPointerException, NumberFormatException {
 		double sum = 0.0;
 		
@@ -24,6 +36,13 @@ public class Ex8_05_4 {
 		}
 				
 		return sum / aryStr.length;
+	}
+	
+	public static void sample()
+			// これは検査例外なので、使う側で捕獲処理が必要になる
+			throws IOException {
+
+		// 処理なし
 	}
 
 }
