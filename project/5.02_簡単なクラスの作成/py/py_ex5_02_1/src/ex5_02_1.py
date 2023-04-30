a76dc532a6dfae5a36a3de6516fdac166e29a99d
kero2no1 <kerokero.2no1@gmail.com>
2023-04-30 11:45:33 +0900

    演習5.3実施(python)

--- project/5.02_簡単なクラスの作成/py/py_ex5_02_1/src/ex5_02_1.py
+++ project/5.02_簡単なクラスの作成/py/py_ex5_02_1/src/ex5_02_1.py
@@ -5,11 +5,17 @@ Created on 2023/04/29
 '''
 
 class Person:
+    # これはクラス変数になる
     name = ""
     age = 0
     income = 0.0
 
 def main():
+    # クラス変数の表示
+    print( Person.name )
+    print( Person.age )
+    print( Person.income )
+    
     p1 = Person()
     p1.name = "keroichi"
     p1.age = 40
@@ -19,5 +25,11 @@ def main():
     print( str(p1.age) + "歳で" )
     print( str(p1.income) + "円の収入です" )
 
+    # もう一度クラス変数の表示
+    # インスタンスで書き換えてもクラス変数は変わらない
+    print( Person.name )
+    print( Person.age )
+    print( Person.income )
+    
 if __name__ == '__main__':
     main()
