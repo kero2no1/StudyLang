136401ef2e3830c2e639c136f7a09301c49cb8b7
kero2no1 <kerokero.2no1@gmail.com>
2023-05-09 14:26:15 +0900

    演習6.2実施(python)

--- project/6.02_継承と変数/py/py_ex6_02_2/src/ex6_02_2.py
+++ project/6.02_継承と変数/py/py_ex6_02_2/src/ex6_02_2.py
@@ -4,8 +4,20 @@ Created on 2023/05/07
 @author: Keroichi.T
 '''
 
+class S:
+    def __init__(self):
+        self.x = 1.0
+        
+class T( S ):
+    def __init__(self):
+        self.x = "1.0"
+
 def main():
-    pass    
+    s = S()
+    t = T()
+    
+    print( f"{s.x}, {type(s.x)}")
+    print( f"{t.x}, {type(t.x)}")
 
 if __name__ == '__main__':
     main()
--- project/6.02_継承と変数/py/py_ex6_02_3/src/ex6_02_3.py
+++ project/6.02_継承と変数/py/py_ex6_02_3/src/ex6_02_3.py
@@ -4,8 +4,28 @@ Created on 2023/05/07
 @author: Keroichi.T
 '''
 
+class G( object ):
+    x = 100
+    
+    def __init__(self):
+        self.y = 200
+    
+class H( G ):
+    x = 10
+    
+    def __init__(self):
+        super().__init__()
+        self.y = 20
+    
+    def display(self):
+        print( f"Hクラスのx:{H.x}" )
+        print( f"Gクラスのx:{super().x}" )
+        print( f"Gクラスのx:{G.x}" )
+
 def main():
-    pass    
+    h = H()
+    
+    h.display()
 
 if __name__ == '__main__':
     main()
