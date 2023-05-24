3d9d1965e130f99efef1b17e5a681a6f644cf678
kero2no1 <kerokero.2no1@gmail.com>
2023-05-24 18:24:44 +0900

    8章の完了(cpp) ※総合理解度チェックはスキップ

---
+++ project/8.EX_総合理解度チェック/cpp/cpp_ex8_ex_2/src/cpp_ex8_ex_2.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : cpp_ex8_ex_2.cpp
+// Author      : keroichi.T
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	/*
+	 * 標準ではスーパークラスを取得する機能はなさそう？
+	 * とりあえずここの演習はスキップ
+	 */
+
+
+	return 0;
+}
